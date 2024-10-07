2e69f633e60df0dd35926aa092fcbcf330f15991
jabaringure@gmail.com <jabaringure@gmail.com>
2024-10-07 17:28:21 +0300

    completes objects lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+const employee = {}
+employee.name = "Brian"
+employee.streetAddress = "Runda Court"
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newEmployee = {...employee}
+    newEmployee[key] = value;
+    return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const anotherNewEmployee = {...employee};
+    delete anotherNewEmployee[key];
+    return anotherNewEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+    delete employee[key];
+    return employee;
+}
